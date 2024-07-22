ab5e027e7c766d6f731915a075cb26c2e2c8b859
Kamala2002 <kamala.surapuram@accellor.com>
2024-07-22 11:25:42 +0530

    Add files via upload

---
+++ PythonBasics.py
@@ -0,0 +1,52 @@
+# print("Welcome Kamala")
+# print("Thank you everyone")
+# print("=========================")
+# parsecs_input = input()
+# parsecs = int(parsecs_input)
+# lightyears = parsecs * 3.26
+# print(parsecs_input+ " parse is "+ str(lightyears) +" lightyears")
+# print("==========================")
+# object_size = 10 
+# proximity = 9000
+# if(object_size > 5 and proximity < 1000):
+#     print("Evasive maneuvers required")
+# else:
+#     print("Object poses no threat")
+# print("===========================")
+# fact = """The "moon" 'has' no atmosphere'.'"""
+# fact += 'No "sound" can be heard on the Moon.'
+# print(fact, end = "okay")
+# print("=======================")
+
+# print("Fact about the Moon:\n There is no atmosphere.\n There is no sound")
+
+# print("""Fact about the Moon:
+#       There is  no atmosphere.
+#       there is no sound.""")
+
+# print("Moon" in "The text describs Facts")
+
+# print("Moon" in "The text describs moon")
+
+# fact = "The highest temperature on Mars is about 30 C".split()
+# for i in fact:
+#     if(i.isdecimal()):
+#         print(i)
+# print("================decimal=======")
+# print("123abd".isdecimal())
+# print(" 123".isdecimal())
+# print("1.23".isdecimal())
+# print("==============digit=========")
+# print("123abd".isdigit())
+# print(" 123".isdigit())
+# print("1.23".isdigit())
+# print("==================numeric========")
+# print("123abd".isnumeric())
+# print(" 123".isnumeric())
+# print("1.23".isnumeric())
+# print("==================")
+# print("-60".startswith('-'))
+# print('S60'.startswith('S'))
+# print("Hello".endswith('o'))
+# print("====================")
+# print("Saturn has a daytime temperature of -170 degrees Celsius, while Mars has -28 Celsius.".replace("Celsius", "C"))
---
+++ open.py
@@ -0,0 +1,4 @@
+def main():
+    open("C:/Users/KamalaSurapuram/OneDrive - Accellor/Desktop".jpg)
+if __name__ == '__main__':
+    main()
